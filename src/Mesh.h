da25c604afa1a910b9a6ee8abc3569d88feca9fa
Smiley98 <smileysofficialcinema@gmail.com>
2025-11-10 15:49:11 -0500

    Added extra practice comments

--- src/Mesh.h
+++ src/Mesh.h
@@ -3,6 +3,13 @@
 #include <vector>
 #include "raymath.h"
 
+// Extra practice 1:
+// Transform the plane within main.cpp's position/tcoord/normal data into this Mesh structure.
+// Add the corresponding vertex buffers to this Mesh structure so it has everything to store a mesh on both the CPU and GPU!
+
+// Extra practice 2:
+// Have a look at the fastObjMesh data-type.
+// See if you can transform the data loaded into fastObjMesh to the data the GPU expects!
 struct Mesh
 {
 	std::vector<Vector3> positions;
