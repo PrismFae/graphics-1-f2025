4e7ba4509bd0402e91d985bd62e9ee0c7c2976b7
Smiley98 <smileysofficialcinema@gmail.com>
2025-11-10 14:48:43 -0500

    Added shader-switching

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,14 @@
 #include <cstdio>
 #include <ctime>
 
+enum ShaderType
+{
+    SHADER_POSITION_COLOR,
+    SHADER_TCOORD_COLOR,
+    SHADER_NORMAL_COLOR,
+    SHADER_TYPE_COUNT
+};
+
 int main()
 {
     // CCW winding order
@@ -43,9 +51,19 @@ int main()
 
     CreateWindow(800, 800, "Graphics 1");
     
-    GLuint position_color_vert = CreateShader(GL_VERTEX_SHADER, "./assets/shaders/tcoord_color.vert");
-    GLuint position_color_frag = CreateShader(GL_FRAGMENT_SHADER, "./assets/shaders/vertex_color.frag");
-    GLuint position_color = CreateProgram(position_color_vert, position_color_frag);
+    GLuint position_color_vert = CreateShader(GL_VERTEX_SHADER, "./assets/shaders/position_color.vert");
+    GLuint tcoord_color_vert = CreateShader(GL_VERTEX_SHADER, "./assets/shaders/tcoord_color.vert");
+    GLuint normal_color_vert = CreateShader(GL_VERTEX_SHADER, "./assets/shaders/normal_color.vert");
+    GLuint vertex_color_frag = CreateShader(GL_FRAGMENT_SHADER, "./assets/shaders/vertex_color.frag");
+
+    GLuint position_color = CreateProgram(position_color_vert, vertex_color_frag);
+    GLuint tcoord_color = CreateProgram(tcoord_color_vert, vertex_color_frag);
+    GLuint normal_color = CreateProgram(normal_color_vert, vertex_color_frag);
+
+    GLuint shaders[SHADER_TYPE_COUNT];
+    shaders[SHADER_POSITION_COLOR] = position_color;
+    shaders[SHADER_TCOORD_COLOR] = tcoord_color;
+    shaders[SHADER_NORMAL_COLOR] = normal_color;
 
     GLuint vbo_plane_positions = CreateBuffer();
     BindVertexBuffer(vbo_plane_positions);
@@ -90,10 +108,14 @@ int main()
 
     UnbindVertexArray(vao_plane);
 
+    int shader_index = 0;
     while (!WindowShouldClose())
     {
         if (IsKeyPressed(KEY_ESCAPE))
             SetWindowShouldClose(true);
+
+        if (IsKeyPressed(KEY_GRAVE_ACCENT))
+            ++shader_index %= SHADER_TYPE_COUNT;
 
         Matrix proj = MatrixOrtho(-1.0f, 1.0f, -1.0f, 1.0f, 0.01f, 100.0f);
         Matrix view = MatrixLookAt({ 0.0f, 0.0f, 10.0f }, { 0.0f, 0.0f, 0.0f }, Vector3UnitY);
@@ -104,7 +126,7 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         BindVertexArray(vao_plane);
-            BeginShader(position_color);
+            BeginShader(shaders[shader_index]);
                 SendMat4(mvp, "u_mvp");
                 glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);  // <-- samples 4 vertices using 6 indices
                 //glDrawArrays(GL_TRIANGLES, 0, 6);                         // <-- requires 6 vertices (not optimal)
@@ -123,8 +145,13 @@ int main()
     DestroyBuffer(&vbo_plane_positions);
 
     DestroyShader(&position_color_vert);
-    DestroyShader(&position_color_frag);
+    DestroyShader(&tcoord_color_vert);
+    DestroyShader(&normal_color_vert);
+    DestroyShader(&vertex_color_frag);
+
     DestroyProgram(&position_color);
+    DestroyProgram(&tcoord_color);
+    DestroyProgram(&normal_color);
 
     DestroyWindow();
     return 0;
